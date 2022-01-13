24de1816ab5d624f88ddcb33c49dc7103d7cabd4
rwema3 <rwemadominik3@gmail.com>
2022-01-13 19:07:05 +0200

    // widgets

--- lib/components/rounded_input_field.dart
+++ lib/components/rounded_input_field.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:Starbelly_Restaurant_app/components/text_field_container.dart';
 import 'package:Starbelly_Restaurant_app/constants.dart';
-
 class RoundedInputField extends StatelessWidget {
   final String hintText;
-  final IconData icon;
+  final IconData icon;// widget  application.
   final ValueChanged<String> onChanged;
   const RoundedInputField({
     Key key,
@@ -12,7 +11,6 @@ class RoundedInputField extends StatelessWidget {
     this.icon = Icons.person,
     this.onChanged,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return TextFieldContainer(
