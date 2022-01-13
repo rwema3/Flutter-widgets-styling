4d8e592dc6362f5d8d321270a46970a546f97a9d
rwema3 <rwemadominik3@gmail.com>
2022-01-13 19:01:44 +0200

    // widget  application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:Starbelly_Restaurant_app/colors.dart';
 import 'Screens/Signup/signup_screen.dart';
-
+// This widget is the root of your application.
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
