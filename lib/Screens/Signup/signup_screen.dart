a84ab5c1875a3e950ae51933e9e38cb4dd4e9bda
rwema3 <rwemadominik3@gmail.com>
2022-01-13 19:15:15 +0200

    Flutter-widgets

--- lib/Screens/Signup/signup_screen.dart
+++ lib/Screens/Signup/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:Starbelly_Restaurant_app/Screens/Signup/components/body.dart';
-
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
