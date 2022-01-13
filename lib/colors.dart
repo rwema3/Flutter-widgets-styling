f20f2da0a40222198a2be9362f30b1744febafdb
rwema3 <rwemadominik3@gmail.com>
2022-01-13 19:05:19 +0200

    // widget  application.

--- lib/colors.dart
+++ lib/colors.dart
@@ -8,5 +8,5 @@ const Ptt = Color(0xFF275F51);
 const Melanje = Color(0xFF234014);
 const pizza = Color(0xFF44B1DC);
 const kaunga = Color(0xFF531B15);
-const Privats = Color(0xFF465C03);
+const Privats = Color(0xFF3B4C07);
 
