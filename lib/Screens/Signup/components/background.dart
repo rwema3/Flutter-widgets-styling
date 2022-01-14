364af0c05482a8ed98797846a833733164eb5a3e
rwema3 <rwemadominik3@gmail.com>
2022-01-14 15:43:52 +0200

    Flutter Widgets-

--- lib/Screens/Signup/components/background.dart
+++ lib/Screens/Signup/components/background.dart
@@ -22,7 +22,7 @@ class Background extends StatelessWidget {
             left: 0,
             child: Image.asset(
               "assets/images/signup_top.png",
-              width: size.width * 0.36,
+              width: size.width * 0.35,
             ),
           ),
           Positioned(
