1e55a1da4422422ae06ed13cab43f3ead4351cf1
rwema3 <rwemadominik3@gmail.com>
2022-01-14 15:32:15 +0200

    Widget build

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'Screens/Signup/signup_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  //This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
