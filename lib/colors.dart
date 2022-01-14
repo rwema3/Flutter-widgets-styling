5187eb5c135007193cf7522fa7344d4defa5bbd5
rwema3 <rwemadominik3@gmail.com>
2022-01-14 15:35:44 +0200

    Flutter Widgets-

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF061807);
 const kPrimaryLightColor = Color(0xFFDBCAE7);
 const Burger = Color(0xFF0E710E);
-const Rice = Color(0xFF074B6F);
+const Rice = Color(0xFF033C5A);
 const Ptt = Color(0xFF275F51);
 const Melanje = Color(0xFF3C5A2C);
 const pizza = Color(0xFF44B1DC);
