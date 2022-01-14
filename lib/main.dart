accc8564c7a5287c48c49f5e5390f642594c196f
rwema3 <rwemadominik3@gmail.com>
2022-01-14 15:31:53 +0200

    Widget build

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Starbelly_Restaurant_app',
       theme: ThemeData(
         primaryColor: kPrimaryColor, scaffoldBackgroundColor: Colors.white,
-      ),/*Flutter theme data*/
+      ),/*Flutter them data*/
       home: SignUpScreen(),
     );
   }// widget  application.
